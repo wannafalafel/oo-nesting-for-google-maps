c4ba20f960de9d3dd8e09479096c6cdcad2dfa51
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-08-18 12:08:17 -0400

    addressed comments

--- spec/location_maker_spec.rb
+++ spec/location_maker_spec.rb
@@ -2,17 +2,28 @@ require_relative 'spec_helper'
 require_relative '../lib/location_maker.rb'
 
 describe LocationMaker do 
+  before(:each) do 
+    LocationMaker.add_location_to_array("Disney World", 28.385233, -81.563874)
+    LocationMaker.add_location_to_array("The Flatiron School", 40.705329, -74.013970)
+  end
+
+  after(:each) do 
+    LocationMaker.class_variable_set(:@@google_maps_array, [])
+  end
+
   describe "#add_location_to_array" do 
     it "takes in three arguments, the name of a location, the latitude and the longitutde of that location "do 
-      LocationMaker.add_location_to_array("The Flatiron School", 40.705329, -74.013970)
-      expect(LocationMaker.map_locations).to include(["The Flatiron School", 40.705329, -74.013970])
+      expect{LocationMaker.add_location_to_array("Disney World", 28.385233, -81.563874)}.to_not raise_error
+    end
+
+    it "successfully adds the new location, as an array, to the @@google_maps_array class variable" do 
+      expect(LocationMaker.class_variable_get(:@@google_maps_array)).to match_array([["Disney World", 28.385233, -81.563874], ["The Flatiron School", 40.705329, -74.013970]])
     end
   end
 
   describe "#map_locations" do 
     it "returns the contents of the google map array constant" do 
-      LocationMaker.add_location_to_array("The Flatiron School", 40.705329, -74.013970)
-      expect(LocationMaker.map_locations).to include(["The Flatiron School", 40.705329, -74.013970])
+      expect(LocationMaker.map_locations).to match_array([["Disney World", 28.385233, -81.563874], ["The Flatiron School", 40.705329, -74.013970]])
     end
   end
 end
