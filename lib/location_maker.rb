7ed5364e63ff953fcbdbb895d5ead8a826333055
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-08-18 12:09:11 -0400

    new solution

--- lib/location_maker.rb
+++ lib/location_maker.rb
@@ -1,17 +1,17 @@
 class LocationMaker
 
-  GOOGLE_MAP_LOCATIONS = []
+  @@google_maps_array = []
 
   def self.add_location_to_array(name, latitude, longitude)
     new_location = []
     new_location[0] = name
     new_location[1] = latitude
     new_location[2] = longitude
-    GOOGLE_MAP_LOCATIONS << new_location
+    @@google_maps_array << new_location
   end
   
   def self.map_locations 
-    GOOGLE_MAP_LOCATIONS
+    @@google_maps_array
   end
 
 end
